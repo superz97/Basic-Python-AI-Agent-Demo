b0f0e1128e9d294ec9965f4c641964378bc514c8
root <root@Zach.>
2025-09-28 18:20:10 +0500

    Implement command line args prompt

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    if len(sys.argv) < 2:
+        print("Error: Please provide a prompt as a command line argument.")
+        print("Usage: uv run main.py \"Your prompt here\"")
+        sys.exit(1)
+    
+    prompt = sys.argv[1]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-main()
+if __name__ == "__main__":
+    main()
