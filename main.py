fa367e50b63fc4b207688d8ce33688aa967468bb
superz97 <b.zangar15@gmail.com>
2025-10-02 20:49:59 +0500

    Redo everything from scratch

--- main.py
+++ main.py
@@ -1,34 +1,73 @@
-import os, sys
-from dotenv import load_dotenv
+import sys
+import os
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
+
+from prompts import system_prompt
+from call_function import call_function, available_functions
+
 
 def main():
-    if len(sys.argv) < 2:
-        print("Error: Please provide a prompt as a command line argument.")
-        print("Usage: uv run main.py \"Your prompt here\" [--verbose]")
-        sys.exit(1)
-    
-    prompt = sys.argv[1]
-    verbose = "--verbose" in sys.argv
-
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
 
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
-
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    user_prompt = " ".join(args)
 
     if verbose:
-        print(f"User prompt: {prompt}")
+        print(f"User prompt: {user_prompt}\n")
 
-    print(response.text)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
     if verbose:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
 
 if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -1,50 +1,8 @@
-from functions.run_python_file import *
+from functions.run_python import run_python_file
 
 
 def test():
-    # result = get_files_info("calculator", ".")
-    # print("Result for current directory:")
-    # print(result)
-    # print("")
-
-    # result = get_files_info("calculator", "pkg")
-    # print("Result for 'pkg' directory:")
-    # print(result)
-
-    # result = get_files_info("calculator", "/bin")
-    # print("Result for '/bin' directory:")
-    # print(result)
-
-    # result = get_files_info("calculator", "../")
-    # print("Result for '../' directory:")
-    # print(result)
-
-
-    # result = get_file_content("calculator", "main.py")
-    # print(result)
-
-    # result = get_file_content("calculator", "pkg/calculator.py")
-    # print(result)
-
-    # result = get_file_content("calculator", "/bin/cat")
-    # print(result)
-
-    # result = get_file_content("calculator", "pkg/does_not_exist.py")
-    # print(result)
-
-    # result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    # print(result)
-
-    # result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    # print(result)
-
-    # result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    # print(result)
-
     result = run_python_file("calculator", "main.py")
-    print(result)
-
-    result = run_python_file("calculator", "main.py", ["3 + 5"])
     print(result)
 
     result = run_python_file("calculator", "tests.py")
